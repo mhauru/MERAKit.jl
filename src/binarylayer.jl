9d549e973bd9e132076e8674147de826d41f7df6
Markus Hauru <markus@mhauru.org>
2020-01-23 15:55:25 +0100

    Flip the index convention.

--- src/binarylayer.jl
+++ src/binarylayer.jl
@@ -55,8 +55,8 @@ Return true/false.
 """
 function space_invar_intralayer(layer::BinaryLayer)
     u, w = layer
-    matching_bonds = [(space(u, 1), space(w, 3)'),
-                      (space(u, 2), space(w, 2)')]
+    matching_bonds = [(space(u, 3)', space(w, 2)),
+                      (space(u, 4)', space(w, 1))]
     allmatch = all([==(pair...) for pair in matching_bonds])
     return allmatch
 end
@@ -68,14 +68,14 @@ disentanglers of the layer above it. Return true/false.
 function space_invar_interlayer(layer::BinaryLayer, next_layer::BinaryLayer)
     u, w = layer.disentangler, layer.isometry
     unext, wnext = next_layer.disentangler, next_layer.isometry
-    matching_bonds = [(space(w, 1), space(unext, 3)'),
-                      (space(w, 1), space(unext, 4)')]
+    matching_bonds = [(space(w, 3)', space(unext, 1)),
+                      (space(w, 3)', space(unext, 2))]
     allmatch = all([==(pair...) for pair in matching_bonds])
     return allmatch
 end
 
-inputspace(layer::BinaryLayer) = space(layer.disentangler, 3)
-outputspace(layer::BinaryLayer) = space(layer.isometry, 1)
+inputspace(layer::BinaryLayer) = space(layer.disentangler, 1)
+outputspace(layer::BinaryLayer) = space(layer.isometry, 3)'
 
 """
 Return a new layer where the isometries have been padded with zeros to change the top vector
@@ -83,7 +83,7 @@ space to be V_new.
 """
 function expand_outputspace(layer::BinaryLayer, V_new)
     u, w = layer
-    w = pad_with_zeros_to(w, 1 => V_new)
+    w = pad_with_zeros_to(w, 3 => V_new')
     return BinaryLayer(u, w)
 end
 
@@ -94,7 +94,7 @@ change the bottom vector space to be V_new.
 function expand_inputspace(layer::BinaryLayer, V_new)
     u, w = layer
     u = pad_with_zeros_to(u, 1 => V_new, 2 => V_new, 3 => V_new', 4 => V_new')
-    w = pad_with_zeros_to(w, 2 => V_new', 3 => V_new')
+    w = pad_with_zeros_to(w, 1 => V_new, 2 => V_new)
     return BinaryLayer(u, w)
 end
 
@@ -110,7 +110,7 @@ function randomlayer(::Type{BinaryLayer}, Vin, Vout; random_disentangler=false)
     u = (random_disentangler ?
          randomisometry(Vin ⊗ Vin, Vin ⊗ Vin)
          : identitytensor(Vin ⊗ Vin, Vin ⊗ Vin))
-    w = randomisometry(Vout, Vin ⊗ Vin)
+    w = randomisometry(Vin ⊗ Vin, Vout)
     return BinaryLayer(u, w)
 end
 
@@ -130,20 +130,20 @@ function ascend(op::SquareTensorMap{3}, layer::BinaryLayer, pos=:avg)
     if in(pos, (:left, :l, :L))
         @tensor(
                 scaled_op[-100,-200,-300,-400,-500,-600] :=
-                w[-100,5,6] * w[-200,9,8] * w[-300,16,15] *
-                u[6,9,1,2] * u[8,16,10,12] *
-                op[1,2,10,3,4,14] *
-                u_dg[3,4,7,13] * u_dg[14,12,11,17] *
-                w_dg[5,7,-400] * w_dg[13,11,-500] * w_dg[17,15,-600]
+                w[5,6,-400] * w[9,8,-500] * w[16,15,-600] *
+                u[1,2,6,9] * u[10,12,8,16] *
+                op[3,4,14,1,2,10] *
+                u_dg[7,13,3,4] * u_dg[11,17,14,12] *
+                w_dg[-100,5,7,] * w_dg[-200,13,11,] * w_dg[-300,17,15]
                )
     elseif in(pos, (:right, :r, :R))
         @tensor(
                 scaled_op[-100,-200,-300,-400,-500,-600] :=
-                w[-100,15,16] * w[-200,8,9] * w[-300,6,5] *
-                u[16,8,12,10] * u[9,6,1,2] *
-                op[10,1,2,14,3,4] *
-                u_dg[12,14,17,11] * u_dg[3,4,13,7] *
-                w_dg[15,17,-400] * w_dg[11,13,-500] * w_dg[7,5,-600]
+                w[15,16,-400] * w[8,9,-500] * w[6,5,-600] *
+                u[12,10,16,8] * u[1,2,9,6] *
+                op[14,3,4,10,1,2] *
+                u_dg[17,11,12,14] * u_dg[13,7,3,4] *
+                w_dg[-100,15,17] * w_dg[-200,11,13] * w_dg[-300,7,5]
                )
     elseif in(pos, (:a, :avg, :average))
         l = ascend(op, layer, :left)
@@ -171,20 +171,20 @@ function ascend(op::TensorMap{S1,3,4,S2,T1,T2,T3}, layer::BinaryLayer, pos=:avg)
     if in(pos, (:left, :l, :L))
         @tensor(
                 scaled_op[-100,-200,-300,-400,-500,-600,-1000] :=
-                w[-100,5,6] * w[-200,9,8] * w[-300,16,15] *
-                u[6,9,1,2] * u[8,16,10,12] *
-                op[1,2,10,3,4,14,-1000] *
-                u_dg[3,4,7,13] * u_dg[14,12,11,17] *
-                w_dg[5,7,-400] * w_dg[13,11,-500] * w_dg[17,15,-600]
+                w[5,6,-400] * w[9,8,-500] * w[16,15,-600] *
+                u[1,2,6,9] * u[10,12,8,16] *
+                op[3,4,14,1,2,10,-1000] *
+                u_dg[7,13,3,4] * u_dg[11,17,14,12] *
+                w_dg[-100,5,7] * w_dg[-200,13,11] * w_dg[-300,17,15]
                )
     elseif in(pos, (:right, :r, :R))
         @tensor(
                 scaled_op[-100,-200,-300,-400,-500,-600,-1000] :=
-                w[-100,15,16] * w[-200,8,9] * w[-300,6,5] *
-                u[16,8,12,10] * u[9,6,1,2] *
-                op[10,1,2,14,3,4,-1000] *
-                u_dg[12,14,17,11] * u_dg[3,4,13,7] *
-                w_dg[15,17,-400] * w_dg[11,13,-500] * w_dg[7,5,-600]
+                w[15,16,-400] * w[8,9,-500] * w[6,5,-600] *
+                u[12,10,16,8] * u[1,2,9,6] *
+                op[14,3,4,10,1,2,-1000] *
+                u_dg[17,11,12,14] * u_dg[13,7,3,4] *
+                w_dg[-100,15,17] * w_dg[-200,11,13] * w_dg[-300,7,5]
                )
     elseif in(pos, (:a, :avg, :average))
         l = ascend(op, layer, :left)
@@ -218,20 +218,20 @@ function descend(rho::SquareTensorMap{3}, layer::BinaryLayer, pos=:avg)
     if in(pos, (:left, :l, :L))
         @tensor(
                 scaled_rho[-100,-200,-300,-400,-500,-600] :=
-                u_dg[-100,-200,16,17] * u_dg[-300,11,2,10] *
-                w_dg[1,16,12] * w_dg[17,2,9] * w_dg[10,4,5] *
-                rho[12,9,5,13,7,6] *
-                w[13,1,14] * w[7,15,3] * w[6,8,4] *
-                u[14,15,-400,-500] * u[3,8,-600,11]
+                u_dg[16,17,-400,-500] * u_dg[2,10,-600,11] *
+                w_dg[12,1,16] * w_dg[9,17,2] * w_dg[5,10,4] *
+                rho[13,7,6,12,9,5] *
+                w[1,14,13] * w[15,3,7] * w[8,4,6] *
+                u[-100,-200,14,15] * u[-300,11,3,8]
                )
     elseif in(pos, (:right, :r, :R))
         @tensor(
                 scaled_rho[-100,-200,-300,-400,-500,-600] :=
-                u_dg[11,-100,10,2] * u_dg[-200,-300,17,16] *
-                w_dg[4,10,5] * w_dg[2,17,9] * w_dg[16,1,12] *
-                rho[5,9,12,6,7,13] *
-                w[6,4,8] * w[7,3,15] * w[13,14,1] *
-                u[8,3,11,-400] * u[15,14,-500,-600]
+                u_dg[10,2,11,-400] * u_dg[17,16,-500,-600] *
+                w_dg[5,4,10] * w_dg[9,2,17] * w_dg[12,16,1] *
+                rho[6,7,13,5,9,12] *
+                w[4,8,6] * w[3,15,7] * w[14,1,13] *
+                u[11,-100,8,3] * u[-200,-300,15,14]
                )
     elseif in(pos, (:a, :avg, :average))
         l = descend(rho, layer, :left)
@@ -250,8 +250,7 @@ end
 Loop over the tensors of the layer, optimizing each one in turn to minimize the expecation
 value of `h`. `rho` is the density matrix right above this layer.
 """
-function minimize_expectation_layer(h, layer::BinaryLayer, rho, pars;
-                                    do_disentanglers=true)
+function minimize_expectation_layer(h, layer::BinaryLayer, rho, pars; do_disentanglers=true)
     for i in 1:pars[:layer_iters]
         if do_disentanglers
             for j in 1:pars[:disentangler_iters]
@@ -275,42 +274,42 @@ function minimize_expectation_disentangler(h::SquareTensorMap{3}, layer::BinaryL
     u_dg = u'
     @tensor(
             env1[-1,-2,-3,-4] :=
-            rho[17,18,10,15,14,9] *
-            w[15,5,6] * w[14,16,-1] * w[9,-2,8] *
-            u[6,16,1,2] *
-            h[1,2,-3,3,4,13] *
-            u_dg[3,4,7,12] * u_dg[13,-4,11,19] *
-            w_dg[5,7,17] * w_dg[12,11,18] * w_dg[19,8,10]
+            rho[15,14,9,17,18,10] *
+            w[5,6,15] * w[16,-3,14] * w[-4,8,9] *
+            u[1,2,6,16] *
+            h[3,4,13,1,2,-1] *
+            u_dg[7,12,3,4] * u_dg[11,19,13,-2] *
+            w_dg[17,5,7] * w_dg[18,12,11] * w_dg[10,19,8]
            )
                 
     @tensor(
             env2[-1,-2,-3,-4] :=
-            rho[4,15,6,3,10,5] *
-            w[3,1,11] * w[10,9,-1] * w[5,-2,2] *
-            u[11,9,12,19] *
-            h[19,-3,-4,18,7,8] *
-            u_dg[12,18,13,14] * u_dg[7,8,16,17] *
-            w_dg[1,13,4] * w_dg[14,16,15] * w_dg[17,2,6]
+            rho[3,10,5,4,15,6] *
+            w[1,11,3] * w[9,-3,10] * w[-4,2,5] *
+            u[12,19,11,9] *
+            h[18,7,8,19,-1,-2] *
+            u_dg[13,14,12,18] * u_dg[16,17,7,8] *
+            w_dg[4,1,13] * w_dg[15,14,16] * w_dg[6,17,2]
            )
                 
     @tensor(
             env3[-1,-2,-3,-4] :=
-            rho[6,15,4,5,10,3] *
-            w[5,2,-1] * w[10,-2,9] * w[3,11,1] *
-            u[9,11,19,12] *
-            h[-3,-4,19,8,7,18] *
-            u_dg[8,7,17,16] * u_dg[18,12,14,13] *
-            w_dg[2,17,6] * w_dg[16,14,15] * w_dg[13,1,4]
+            rho[5,10,3,6,15,4] *
+            w[2,-3,5] * w[-4,9,10] * w[11,1,3] *
+            u[19,12,9,11] *
+            h[8,7,18,-1,-2,19] *
+            u_dg[17,16,8,7] * u_dg[14,13,18,12] *
+            w_dg[6,2,17] * w_dg[15,16,14] * w_dg[4,13,1]
            )
 
     @tensor(
             env4[-1,-2,-3,-4] :=
-            rho[10,18,17,9,14,15] *
-            w[9,8,-1] * w[14,-2,16] * w[15,6,5] *
-            u[16,6,2,1] *
-            h[-4,2,1,13,4,3] *
-            u_dg[-3,13,19,11] * u_dg[4,3,12,7] *
-            w_dg[8,19,10] * w_dg[11,12,18] * w_dg[7,5,17]
+            rho[9,14,15,10,18,17] *
+            w[8,-3,9] * w[-4,16,14] * w[6,5,15] *
+            u[2,1,16,6] *
+            h[13,4,3,-2,2,1] *
+            u_dg[19,11,-1,13] * u_dg[12,7,4,3] *
+            w_dg[10,8,19] * w_dg[18,11,12] * w_dg[17,7,5]
            )
 
     env = env1 + env2 + env3
@@ -331,7 +330,6 @@ function minimize_expectation_disentangler(h::SquareTensorMap{1}, layer::BinaryL
     return minimize_expectation_disentangler(h, layer, rho)
 end
 
-
 """
 Return a new layer, where the isometry has been changed to the locally optimal one to
 minimize the expectation of a threesite operator `h`.
@@ -342,68 +340,68 @@ function minimize_expectation_isometry(h::SquareTensorMap{3}, layer::BinaryLayer
     u_dg = u'
     @tensor(
             env1[-1,-2,-3] :=
-            rho[16,15,19,18,17,-1] *
-            w[18,5,6] * w[17,9,8] *
-            u[6,9,2,1] * u[8,-2,10,11] *
-            h[2,1,10,4,3,12] *
-            u_dg[4,3,7,14] * u_dg[12,11,13,20] *
-            w_dg[5,7,16] * w_dg[14,13,15] * w_dg[20,-3,19]
+            rho[18,17,-3,16,15,19] *
+            w[5,6,18] * w[9,8,17] *
+            u[2,1,6,9] * u[10,11,8,-1] *
+            h[4,3,12,2,1,10] *
+            u_dg[7,14,4,3] * u_dg[13,20,12,11] *
+            w_dg[16,5,7] * w_dg[15,14,13] * w_dg[19,20,-2]
            )
                 
     @tensor(
             env2[-1,-2,-3] :=
-            rho[18,17,19,16,15,-1] *
-            w[16,12,13] * w[15,5,6] *
-            u[13,5,9,7] * u[6,-2,2,1] *
-            h[7,2,1,8,4,3] *
-            u_dg[9,8,14,11] * u_dg[4,3,10,20] *
-            w_dg[12,14,18] * w_dg[11,10,17] * w_dg[20,-3,19]
+            rho[16,15,-3,18,17,19] *
+            w[12,13,16] * w[5,6,15] *
+            u[9,7,13,5] * u[2,1,6,-1] *
+            h[8,4,3,7,2,1] *
+            u_dg[14,11,9,8] * u_dg[10,20,4,3] *
+            w_dg[18,12,14] * w_dg[17,11,10] * w_dg[19,20,-2]
            )
 
     @tensor(
             env3[-1,-2,-3] :=
-            rho[19,20,15,18,-1,14] *
-            w[18,5,6] * w[14,17,13] *
-            u[6,-2,2,1] * u[-3,17,12,11] *
-            h[2,1,12,4,3,9] *
-            u_dg[4,3,7,10] * u_dg[9,11,8,16] *
-            w_dg[5,7,19] * w_dg[10,8,20] * w_dg[16,13,15]
+            rho[18,-3,14,19,20,15] *
+            w[5,6,18] * w[17,13,14] *
+            u[2,1,6,-1] * u[12,11,-2,17] *
+            h[4,3,9,2,1,12] *
+            u_dg[7,10,4,3] * u_dg[8,16,9,11] *
+            w_dg[19,5,7] * w_dg[20,10,8] * w_dg[15,16,13]
            )
 
     @tensor(
             env4[-1,-2,-3] :=
-            rho[15,20,19,14,-1,18] *
-            w[14,13,17] * w[18,6,5] *
-            u[17,-2,11,12] * u[-3,6,1,2] *
-            h[12,1,2,9,3,4] *
-            u_dg[11,9,16,8] * u_dg[3,4,10,7] *
-            w_dg[13,16,15] * w_dg[8,10,20] * w_dg[7,5,19]
+            rho[14,-3,18,15,20,19] *
+            w[13,17,14] * w[6,5,18] *
+            u[11,12,17,-1] * u[1,2,-2,6] *
+            h[9,3,4,12,1,2] *
+            u_dg[16,8,11,9] * u_dg[10,7,3,4] *
+            w_dg[15,13,16] * w_dg[20,8,10] * w_dg[19,7,5]
            )
 
     @tensor(
             env5[-1,-2,-3] :=
-            rho[19,17,18,-1,15,16] *
-            w[15,6,5] * w[16,13,12] *
-            u[-3,6,1,2] * u[5,13,7,9] *
-            h[1,2,7,3,4,8] *
-            u_dg[3,4,20,10] * u_dg[8,9,11,14] *
-            w_dg[-2,20,19] * w_dg[10,11,17] * w_dg[14,12,18]
+            rho[-3,15,16,19,17,18] *
+            w[6,5,15] * w[13,12,16] *
+            u[1,2,-2,6] * u[7,9,5,13] *
+            h[3,4,8,1,2,7] *
+            u_dg[20,10,3,4] * u_dg[11,14,8,9] *
+            w_dg[19,-1,20] * w_dg[17,10,11] * w_dg[18,14,12]
            )
 
     @tensor(
             env6[-1,-2,-3] :=
-            rho[19,15,16,-1,17,18] *
-            w[17,8,9] * w[18,6,5] *
-            u[-3,8,11,10] * u[9,6,1,2] *
-            h[10,1,2,12,3,4] *
-            u_dg[11,12,20,13] * u_dg[3,4,14,7] *
-            w_dg[-2,20,19] * w_dg[13,14,15] * w_dg[7,5,16]
+            rho[-3,17,18,19,15,16] *
+            w[8,9,17] * w[6,5,18] *
+            u[11,10,-2,8] * u[1,2,9,6] *
+            h[12,3,4,10,1,2] *
+            u_dg[20,13,11,12] * u_dg[14,7,3,4] *
+            w_dg[19,-1,20] * w_dg[15,13,14] * w_dg[16,7,5]
            )
 
     env = env1 + env2 + env3 + env4 + env5 + env6
-    U, S, Vt = svd(env, (1,), (2,3))
-    @tensor w[-1,-2,-3] := conj(U[-1,1]) * conj(Vt[1,-2,-3])
-    w = permuteind(w, (1,), (2,3))
+    U, S, Vt = svd(env, (1,2), (3,))
+    @tensor w[-1,-2,-3] := conj(U[-1,-2,1]) * conj(Vt[1,-3])
+    w = permuteind(w, (1,2), (3,))
     return BinaryLayer(u, w)
 end
 
--- src/ternarylayer.jl
+++ src/ternarylayer.jl
@@ -55,8 +55,8 @@ Return true/false.
 """
 function space_invar_intralayer(layer::TernaryLayer)
     u, w = layer
-    matching_bonds = [(space(u, 1), space(w, 4)'),
-                      (space(u, 2), space(w, 2)')]
+    matching_bonds = [(space(u, 3)', space(w, 3)),
+                      (space(u, 4)', space(w, 1))]
     allmatch = all([==(pair...) for pair in matching_bonds])
     return allmatch
 end
@@ -68,14 +68,14 @@ disentanglers of the layer above it. Return true/false.
 function space_invar_interlayer(layer::TernaryLayer, next_layer::TernaryLayer)
     u, w = layer.disentangler, layer.isometry
     unext, wnext = next_layer.disentangler, next_layer.isometry
-    matching_bonds = [(space(w, 1), space(unext, 3)'),
-                      (space(w, 1), space(unext, 4)')]
+    matching_bonds = [(space(w, 4)', space(unext, 1)),
+                      (space(w, 4)', space(unext, 2))]
     allmatch = all([==(pair...) for pair in matching_bonds])
     return allmatch
 end
 
-inputspace(layer::TernaryLayer) = space(layer.disentangler, 3)
-outputspace(layer::TernaryLayer) = space(layer.isometry, 1)
+inputspace(layer::TernaryLayer) = space(layer.disentangler, 1)
+outputspace(layer::TernaryLayer) = space(layer.isometry, 4)'
 
 """
 Return a new layer where the isometries have been padded with zeros to change the top vector
@@ -83,7 +83,7 @@ space to be V_new.
 """
 function expand_outputspace(layer::TernaryLayer, V_new)
     u, w = layer
-    w = pad_with_zeros_to(w, 1 => V_new)
+    w = pad_with_zeros_to(w, 4 => V_new')
     return TernaryLayer(u, w)
 end
 
@@ -94,7 +94,7 @@ change the bottom vector space to be V_new.
 function expand_inputspace(layer::TernaryLayer, V_new)
     u, w = layer
     u = pad_with_zeros_to(u, 1 => V_new, 2 => V_new, 3 => V_new', 4 => V_new')
-    w = pad_with_zeros_to(w, 2 => V_new', 3 => V_new', 4 => V_new')
+    w = pad_with_zeros_to(w, 1 => V_new, 2 => V_new, 3 => V_new)
     return TernaryLayer(u, w)
 end
 
@@ -110,7 +110,7 @@ function randomlayer(::Type{TernaryLayer}, Vin, Vout; random_disentangler=false)
     u = (random_disentangler ?
          randomisometry(Vin ⊗ Vin, Vin ⊗ Vin)
          : identitytensor(Vin ⊗ Vin, Vin ⊗ Vin))
-    w = randomisometry(Vout, Vin ⊗ Vin ⊗ Vin)
+    w = randomisometry(Vin ⊗ Vin ⊗ Vin, Vout)
     return TernaryLayer(u, w)
 end
 
@@ -130,7 +130,7 @@ ascending_superop_onesite(m::TernaryMERA) = ascending_superop_onesite(get_layer(
 function ascending_superop_onesite(layer::TernaryLayer)
     w = layer.isometry
     w_dg = w'
-    @tensor(superop[-1,-2,-11,-12] := w[-1,1,-11,2] * w_dg[1,-12,2,-2])
+    @tensor(superop[-1,-2,-11,-12] := w[1,-2,2,-12] * w_dg[-11,1,-1,2])
     return superop
 end
 
@@ -145,31 +145,31 @@ function ascend(op::SquareTensorMap{2}, layer::TernaryLayer, pos=:avg)
         # Cost: 2X^8 + 2X^7 + 2X^6
         @tensor(
                 scaled_op[-100,-200,-300,-400] :=
-                w[-100,51,52,53] * w[-200,54,11,12] *
-                u[53,54,41,42] *
-                op[52,41,31,32] *
-                u_dg[32,42,21,55] *
-                w_dg[51,31,21,-300] * w_dg[55,11,12,-400]
+                w[51,52,53,-300,] * w[54,11,12,-400] *
+                u[41,42,53,54] *
+                op[31,32,52,41] *
+                u_dg[21,55,32,42] *
+                w_dg[-100,51,31,21] * w_dg[-200,55,11,12]
                )
     elseif in(pos, (:right, :r, :R))
         # Cost: 2X^8 + 2X^7 + 2X^6
         @tensor(
                 scaled_op[-100,-200,-300,-400] :=
-                w[-100,11,12,65] * w[-200,63,61,62] *
-                u[65,63,51,52] *
-                op[52,61,31,41] *
-                u_dg[51,31,64,21] *
-                w_dg[11,12,64,-300] * w_dg[21,41,62,-400]
+                w[11,12,65,-300] * w[63,61,62,-400] *
+                u[51,52,65,63] *
+                op[31,41,52,61] *
+                u_dg[64,21,51,31] *
+                w_dg[-100,11,12,64] * w_dg[-200,21,41,62]
                )
     elseif in(pos, (:middle, :mid, :m, :M))
         # Cost: 6X^6
         @tensor(
                 scaled_op[-100,-200,-300,-400] :=
-                w[-100,31,32,41] * w[-200,51,21,22] *
-                u[41,51,1,2] *
-                op[1,2,11,12] *
-                u_dg[11,12,42,52] *
-                w_dg[31,32,42,-300] * w_dg[52,21,22,-400]
+                w[31,32,41,-300] * w[51,21,22,-400] *
+                u[1,2,41,51] *
+                op[11,12,1,2] *
+                u_dg[42,52,11,12] *
+                w_dg[-100,31,32,42] * w_dg[-200,52,21,22]
                )
     elseif in(pos, (:a, :avg, :average))
         l = ascend(op, layer, :l)
@@ -197,31 +197,31 @@ function ascend(op::TensorMap{S1,2,3,S2,T1,T2,T3}, layer::TernaryLayer, pos=:avg
         # Cost: 2X^8 + 2X^7 + 2X^6
         @tensor(
                 scaled_op[-100,-200,-300,-400,-1000] :=
-                w[-100,51,52,53] * w[-200,54,11,12] *
-                u[53,54,41,42] *
-                op[52,41,31,32,-1000] *
-                u_dg[32,42,21,55] *
-                w_dg[51,31,21,-300] * w_dg[55,11,12,-400]
+                w[51,52,53,-300] * w[54,11,12,-400] *
+                u[41,42,53,54] *
+                op[31,32,52,41,-1000] *
+                u_dg[21,55,32,42] *
+                w_dg[-100,51,31,21] * w_dg[-200,55,11,12]
                )
     elseif in(pos, (:right, :r, :R))
         # Cost: 2X^8 + 2X^7 + 2X^6
         @tensor(
                 scaled_op[-100,-200,-300,-400,-1000] :=
-                w[-100,11,12,65] * w[-200,63,61,62] *
-                u[65,63,51,52] *
-                op[52,61,31,41,-1000] *
-                u_dg[51,31,64,21] *
-                w_dg[11,12,64,-300] * w_dg[21,41,62,-400]
+                w[11,12,65,-300] * w[63,61,62,-400] *
+                u[51,52,65,63] *
+                op[31,41,52,61,-1000] *
+                u_dg[64,21,51,31] *
+                w_dg[-100,11,12,64] * w_dg[-200,21,41,62]
                )
     elseif in(pos, (:middle, :mid, :m, :M))
         # Cost: 6X^6
         @tensor(
                 scaled_op[-100,-200,-300,-400,-1000] :=
-                w[-100,31,32,41] * w[-200,51,21,22] *
-                u[41,51,1,2] *
-                op[1,2,11,12,-1000] *
-                u_dg[11,12,42,52] *
-                w_dg[31,32,42,-300] * w_dg[52,21,22,-400]
+                w[31,32,41,-300] * w[51,21,22,-400] *
+                u[1,2,41,51] *
+                op[11,12,1,2,-1000] *
+                u_dg[42,52,11,12] *
+                w_dg[-100,31,32,42] * w_dg[-200,52,21,22]
                )
     elseif in(pos, (:a, :avg, :average))
         l = ascend(op, layer, :l)
@@ -252,31 +252,31 @@ function descend(rho::SquareTensorMap{2}, layer::TernaryLayer, pos=:avg)
         # Cost: 2X^8 + 2X^7 + 2X^6
         @tensor(
                 scaled_rho[-100,-200,-300,-400] :=
-                u_dg[-200,63,61,62] *
-                w_dg[52,-100,61,51] * w_dg[62,11,12,21] *
-                rho[51,21,42,22] *
-                w[42,52,-300,41] * w[22,31,11,12] *
-                u[41,31,-400,63]
+                u_dg[61,62,-400,63] *
+                w_dg[51,52,-300,61] * w_dg[21,62,11,12] *
+                rho[42,22,51,21] *
+                w[52,-100,41,42] * w[31,11,12,22] *
+                u[-200,63,41,31]
                )
     elseif in(pos, (:right, :r, :R))
         # Cost: 2X^8 + 2X^7 + 2X^6
         @tensor(
                 scaled_rho[-100,-200,-300,-400] :=
-                u_dg[63,-100,62,61] *
-                w_dg[11,12,62,21] * w_dg[61,-200,52,51] *
-                rho[21,51,22,42] *
-                w[22,11,12,41] * w[42,31,-400,52] *
-                u[41,31,63,-300]
+                u_dg[62,61,63,-300] *
+                w_dg[21,11,12,62] * w_dg[51,61,-400,52] *
+                rho[22,42,21,51] *
+                w[11,12,41,22] * w[31,-200,52,42] *
+                u[63,-100,41,31]
                )
     elseif in(pos, (:middle, :mid, :m, :M))
         # Cost: 6X^6
         @tensor(
                 scaled_rho[-100,-200,-300,-400] :=
-                u_dg[-100,-200,61,62] *
-                w_dg[11,12,61,21] * w_dg[62,31,32,41] *
-                rho[21,41,22,42] *
-                w[22,11,12,51] * w[42,52,31,32] *
-                u[51,52,-300,-400]
+                u_dg[61,62,-300,-400] *
+                w_dg[21,11,12,61] * w_dg[41,62,31,32] *
+                rho[22,42,21,41] *
+                w[11,12,51,22] * w[52,31,32,42] *
+                u[-100,-200,51,52]
                )
     elseif in(pos, (:a, :avg, :average))
         l = descend(rho, layer, :l)
@@ -322,31 +322,31 @@ function minimize_expectation_disentangler(h::SquareTensorMap{2}, layer::Ternary
     # Cost: 2X^8 + 2X^7 + 2X^6
     @tensor(
             env1[-1,-2,-3,-4] :=
-            rho[31,21,63,22] *
-            w[63,61,62,-1] * w[22,-2,11,12] *
-            h[62,-3,51,52] *
-            u_dg[52,-4,41,42] *
-            w_dg[61,51,41,31] * w_dg[42,11,12,21]
+            rho[63,22,31,21] *
+            w[61,62,-3,63] * w[-4,11,12,22] *
+            h[51,52,62,-1] *
+            u_dg[41,42,52,-2] *
+            w_dg[31,61,51,41] * w_dg[21,42,11,12]
            )
 
     # Cost: 6X^6
     @tensor(
             env2[-1,-2,-3,-4] :=
-            rho[41,51,42,52] *
-            w[42,21,22,-1] * w[52,-2,31,32] *
-            h[-3,-4,11,12] *
-            u_dg[11,12,61,62] *
-            w_dg[21,22,61,41] * w_dg[62,31,32,51]
+            rho[42,52,41,51] *
+            w[21,22,-3,42] * w[-4,31,32,52] *
+            h[11,12,-1,-2] *
+            u_dg[61,62,11,12] *
+            w_dg[41,21,22,61] * w_dg[51,62,31,32]
            )
 
     # Cost: 2X^8 + 2X^7 + 2X^6
     @tensor(
             env3[-1,-2,-3,-4] :=
-            rho[21,31,22,63] *
-            w[22,12,11,-1] * w[63,-2,62,61] *
-            h[-4,62,52,51] *
-            u_dg[-3,52,42,41] *
-            w_dg[12,11,42,21] * w_dg[41,51,61,31]
+            rho[22,63,21,31] *
+            w[12,11,-3,22] * w[-4,62,61,63] *
+            h[52,51,-2,62] *
+            u_dg[42,41,-1,52] *
+            w_dg[21,12,11,42] * w_dg[31,41,51,61]
            )
 
     env = env1 + env2 + env3
@@ -367,73 +367,73 @@ function minimize_expectation_isometry(h::SquareTensorMap{2}, layer::TernaryLaye
     # Cost: 2X^8 + 2X^7 + 2X^6
     @tensor(
             env1[-1,-2,-3,-4] :=
-            rho[81,84,82,-1] *
-            w[82,62,61,63] *
-            u[63,-2,51,52] *
-            h[61,51,41,42] *
-            u_dg[42,52,31,83] *
-            w_dg[62,41,31,81] * w_dg[83,-3,-4,84]
+            rho[82,-4,81,84] *
+            w[62,61,63,82] *
+            u[51,52,63,-1] *
+            h[41,42,61,51] *
+            u_dg[31,83,42,52] *
+            w_dg[81,62,41,31] * w_dg[84,83,-2,-3]
            )
 
     # Cost: 6X^6
     @tensor(
             env2[-1,-2,-3,-4] :=
-            rho[41,62,42,-1] *
-            w[42,11,12,51] *
-            u[51,-2,21,22] *
-            h[21,22,31,32] *
-            u_dg[31,32,52,61] *
-            w_dg[11,12,52,41] * w_dg[61,-3,-4,62]
+            rho[42,-4,41,62] *
+            w[11,12,51,42] *
+            u[21,22,51,-1] *
+            h[31,32,21,22] *
+            u_dg[52,61,31,32] *
+            w_dg[41,11,12,52] * w_dg[62,61,-2,-3]
            )
 
     # Cost: 2X^8 + 2X^7 + 2X^6
     @tensor(
             env3[-1,-2,-3,-4] :=
-            rho[31,33,32,-1] *
-            w[32,21,11,73] *
-            u[73,-2,72,71] *
-            h[71,-3,62,61] *
-            u_dg[72,62,51,41] *
-            w_dg[21,11,51,31] * w_dg[41,61,-4,33]
+            rho[32,-4,31,33] *
+            w[21,11,73,32] *
+            u[72,71,73,-1] *
+            h[62,61,71,-2] *
+            u_dg[51,41,72,62] *
+            w_dg[31,21,11,51] * w_dg[33,41,61,-3]
            )
 
     # Cost: 2X^8 + 2X^7 + 2X^6
     @tensor(
             env4[-1,-2,-3,-4] :=
-            rho[33,31,-1,32] *
-            w[32,73,11,21] *
-            u[-4,73,71,72] *
-            h[-3,71,61,62] *
-            u_dg[62,72,41,51] *
-            w_dg[-2,61,41,33] * w_dg[51,11,21,31]
+            rho[-4,32,33,31] *
+            w[73,11,21,32] *
+            u[71,72,-3,73] *
+            h[61,62,-2,71] *
+            u_dg[41,51,62,72] *
+            w_dg[33,-1,61,41] * w_dg[31,51,11,21]
            )
 
     # Cost: 6X^6
     @tensor(
             env5[-1,-2,-3,-4] :=
-            rho[62,41,-1,42] *
-            w[42,51,12,11] *
-            u[-4,51,22,21] *
-            h[22,21,32,31] *
-            u_dg[32,31,61,52] *
-            w_dg[-2,-3,61,62] * w_dg[52,12,11,41]
+            rho[-4,42,62,41] *
+            w[51,12,11,42] *
+            u[22,21,-3,51] *
+            h[32,31,22,21] *
+            u_dg[61,52,32,31] *
+            w_dg[62,-1,-2,61] * w_dg[41,52,12,11]
            )
 
     # Cost: 2X^8 + 2X^7 + 2X^6
     @tensor(
             env6[-1,-2,-3,-4] :=
-            rho[84,81,-1,82] *
-            w[82,63,61,62] *
-            u[-4,63,52,51] *
-            h[51,61,42,41] *
-            u_dg[52,42,83,31] *
-            w_dg[-2,-3,83,84] * w_dg[31,41,62,81]
+            rho[-4,82,84,81] *
+            w[63,61,62,82] *
+            u[52,51,-3,63] *
+            h[42,41,51,61] *
+            u_dg[83,31,52,42] *
+            w_dg[84,-1,-2,83] * w_dg[81,31,41,62]
            )
 
     env = env1 + env2 + env3 + env4 + env5 + env6
-    U, S, Vt = svd(env, (1,), (2,3,4))
-    @tensor w[-1,-2,-3,-4] := conj(U[-1,1]) * conj(Vt[1,-2,-3,-4])
-    w = permuteind(w, (1,), (2,3,4))
+    U, S, Vt = svd(env, (1,2,3), (4,))
+    @tensor w[-1,-2,-3,-4] := conj(U[-1,-2,-3,1]) * conj(Vt[1,-4])
+    w = permuteind(w, (1,2,3), (4,))
     return TernaryLayer(u, w)
 end
 
