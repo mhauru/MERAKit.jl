5cefca54507e89546d8c17fd15bcc5ed3696f7d5
Markus Hauru <markus@mhauru.org>
2020-08-19 16:39:47 +0200

    Docstring for `baselayertype(::Type{<:Layer})`

--- src/layer.jl
+++ src/layer.jl
@@ -7,6 +7,13 @@ Abstract supertype of all layer types, e.g. `BinaryLayer` and `TernaryLayer`.
 abstract type Layer end
 
 layertype(::Type{L}) where {L<:Layer} = L
+
+"""
+    baselayertype(::Type{<:Layer})
+
+Return the unparametrised layer type, e.g. for `BinaryLayer{ComplexSpace, Float64, false}`,
+return just `BinaryLayer`.
+"""
 baselayertype(::Type{L}) where {L<:Layer} = Base.typename(L).wrapper
 
 """
