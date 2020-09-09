fbaaacf83290525506d6c94b4d01033a9a987032
Markus Hauru <markus@mhauru.org>
2020-09-09 13:18:19 +0200

    Completely rework demo

--- demo/demo.jl
+++ demo/demo.jl
@@ -1,132 +1,117 @@
-# A script that optimizes a MERA for either the Ising or XXZ model, and computes some
-# physical quantities from it. The MERAs built are stored on disk.
-
-using ArgParse
+# This script demonstrates how to use MERA.jl to find an approximation to the ground state
+# of the critical Ising model.
+using Random
 using LinearAlgebra
 using TensorKit
+using MERA
+# DemoTools holds some utility functions needed by this script, for creating Hamiltonians
+# and storing and reading MERAs to/from disk. It exports nothing, so all calls need to be
+# qualified as DemoTools.whatever.
 include("demo_tools.jl")
 using .DemoTools
 
-function parse_pars()
-    settings = ArgParseSettings(autofix_names=true)
-    @add_arg_table!(settings
-                    , "--model", arg_type=String, default="Ising"
-                    , "--meratype", arg_type=String, default="ternary"
-                    , "--threads", arg_type=Int, default=1  # For BLAS parallelization
-                    , "--chi", arg_type=Int, default=8  # Max bond dimension
-                    , "--layers", arg_type=Int, default=3
-                    , "--symmetry", arg_type=String, default="none"  # "none" or "group"
-                    , "--block_size", arg_type=Int, default=2  # Block two sites to start
-                    , "--h", arg_type=Float64, default=1.0  # External field of Ising
-                    , "--J_z", arg_type=Float64, default=0.5  # ZZ coupling for XXZ
-                    , "--J_xy", arg_type=Float64, default=-1.0  # XX + YY coupling for XXZ
-                    , "--datafolder", arg_type=String, default="JLMdata"
-                    , "--method", arg_type=Symbol, default=:lbfgs
-                    , "--retraction", arg_type=Symbol, default=:exp
-                    , "--transport", arg_type=Symbol, default=:exp
-                    , "--metric", arg_type=Symbol, default=:euclidean
-                    , "--precondition", arg_type=Bool, default=true
-                    , "--lbfgs-m", arg_type=Int, default=8
-                    , "--cg-flavor", arg_type=Symbol, default=:HagerZhang
-                    , "--scale_invariant_eps", arg_type=Float64, default=1e-6
-                    , "--verbosity", arg_type=Int, default=2
-                   )
-    pars = parse_args(ARGS, settings; as_symbols=true)
-    return pars
-end
-
 function main()
-    pars = parse_pars()
-    model = pars[:model]
-    chi = pars[:chi]
-    layers = pars[:layers]
-    symmetry = pars[:symmetry]
-    datafolder = pars[:datafolder]
-    if symmetry == "group"
-        model == "Ising" && (symmetry = "Z2")
-        model == "XXZ" && (symmetry = "U1")
-        pars[:symmetry] = symmetry
-    end
-    block_size = pars[:block_size]
+    # Fix the random seed for the sake of reproducibility. MERA algorithms sometimes get
+    # stuck in local minima, so you should usually try a few different seeds for the same
+    # optimisation. This one is going to do well though.
+    Random.seed!(1)
 
-    logstr = "Running demo.jl with"
-    for (k, v) in pars
-        logstr = logstr * "\n$k = $v"
-    end
-    @info(logstr)
+    # Create the local Hamiltonian term `h` for the critical Ising model. Use Z2 symmetric
+    # tensors, and block two sites together, so that the local state space dimension is 4.
+    # The blocking simply obviates the need for a trivial, unitary layer at the bottom of
+    # the MERA.
+    h = DemoTools.ising_hamiltonian(1; symmetry = :Z2, block_size = 2)
+    exact_energy = -4/pi
+    V_physical = space(h, 1)
 
-    # Three sets of parameters are used when optimizing the MERA:
-    # Used when determining which sector to give bond dimension to.
-    scale_invariant_eps = pars[:scale_invariant_eps]
-    pars[:initial_opt_pars] = (
-                               maxiter = 30,
-                               isometries_only_iters = 10,
-                               method = pars[:method],
-                               retraction = pars[:retraction],
-                               transport = pars[:transport],
-                               metric = pars[:metric],
-                               precondition = pars[:precondition],
-                               lbfgs_m = pars[:lbfgs_m],
-                               cg_flavor = pars[:cg_flavor],
-                               verbosity = pars[:verbosity],
-                               scaleinvariant_krylovoptions = (
-                                                               tol = scale_invariant_eps,
-                                                               verbosity = 0,
-                                                               maxiter = 20,
-                                                              ),
-                              )
-    # Used when optimizing a MERA that has some layers expanded to desired bond dimension,
-    # but not all.
-    pars[:mid_opt_pars] = merge(pars[:initial_opt_pars],
-                                (maxiter = 100, isometry_iters = 0))
-    # Used when optimizing a MERA that has all bond dimensions at the full, desired value.
-    pars[:final_opt_pars] = merge(pars[:initial_opt_pars],
-                                  (gradient_delta = 1e-7,
-                                   maxiter = 1000,
-                                   isometry_iters = 0))
+    # Choose the type of MERA to use.
+    meratype = TernaryMERA
+    layers = 3
+    # It's typically beneficial to gradually increase the bond dimension during the
+    # optimisation. We do this here using the following steps: Each element of
+    # `V_virtual_steps` is a vector space for the virtual indices of the MERA, and we run
+    # the optimisation for each of them, using as the starting point the result of the
+    # previous step.
+    # Note that for Z2 symmetric tensors we have to specify how the bond dimension is
+    # divided between the symmetry sectors. It is a priori not obvious what the optimal
+    # choice is, and although here it happens to be such that both sectors get the same
+    # dimension, this isn't always the case. In general, finding the optimal proportions
+    # requires manually trying different options.
+    # Note also that we could use different vector spaces for the different indices in the
+    # MERA, both the ones between layers and the ones within layers (between isometries and
+    # disentanglers, making the disentanglers isometric instead of unitary). This would
+    # probably be better for accuracy/speed, but using the same `V_virtual` everywhere works
+    # well too, so for simplicity we stick to that.
+    V_virtual_steps = (Z2Space(0 => 2, 1 => 2),
+                       Z2Space(0 => 3, 1 => 3),
+                       Z2Space(0 => 4, 1 => 4))
 
-    # Get the Hamiltonian.
-    if model == "Ising"
-        h = DemoTools.build_H_Ising(pars[:h]; symmetry=symmetry,
-                                    block_size=block_size)
-        symmetry == "none" && (magop = DemoTools.build_magop(block_size=block_size))
-    elseif model == "XXZ"
-        h = DemoTools.build_H_XXZ(pars[:J_xy], pars[:J_z]; symmetry=symmetry,
-                                  block_size=block_size)
-    else
-        msg = "Unknown model $(model)."
-        throw(ArgumentError(msg))
-    end
+    # Specify the optimisation method. We use here Riemannian LBFGS as described in
+    # https://arxiv.org/abs/2007.03638
+    method = :lbfgs
+    verbosity = 2
+    gradient_delta = 1e-7
+    # In practice the above threshold for the gradient norm, which determines convergence,
+    # will not be reached. Instead, we set the maximum number of iterations to be done. We
+    # specify this separately for the different bond dimensions in `V_virtual_steps`.
+    # Choosing these numbers is more art than science, but many different choices would work
+    # roughly equally well.
+    maxiter_steps = (1000, 500, 500)
 
-    if symmetry == "none"
-        chis = 1:chi
-    else
-        model == "Ising" && (chis = 2:chi)
-        model == "XXZ" && (chis = 3:chi)
-    end
+    # Once the optimisation is done, the resulting MERA will be written to a file at this
+    # path.
+    path = "./demo_result.jld2"
 
-    # Computing the magnetisation of the Ising model only makes sense if Z2 symmetry isn't
-    # explicitly enforced.
-    do_magnetisation = model == "Ising" && symmetry == "none"
-    for chi in chis
-        # For each bond dimension, get the optimized MERA and print out some interesting
-        # numbers for it.
-        temppars = deepcopy(pars)
-        temppars[:chi] = chi
-        m = DemoTools.get_optimized_mera(datafolder, model, temppars)
-
-        energy = expect(h, m)
+    # To keep track of how the optimisation is progressing, we will every once in a while
+    # print out the energy error, the entropies of the density matrices at different scales
+    # in the MERA, and the scaling dimensions. For this we use the `finalize!` keyword
+    # argument of `minimize_expectation`, which can be a function that is run at the end of
+    # every iteration. `finalize!` could be used to do plenty of things, including modifying
+    # the current MERA `m` during the optimisation, but we simply use it to print things out
+    # every `checkpoint_frequency` iterations. See the documentation of MERA.jl for more
+    # details on `finalize!`.
+    checkpoint_frequency = 100
+    function finalize!(m, energy, g, repnum)
+        repnum % checkpoint_frequency != 0 && (return m, energy, g)
         rhoees = densitymatrix_entropies(m)
-        do_magnetisation && (magnetization = expect(magop, remove_symmetry(m)))
         scaldims = scalingdimensions(m)
-
-        @info("Done with bond dimension $(chi).")
-        @info("Energy numerical: $energy")
-        model == "Ising" && @info("Energy exact:     $(-4/pi)")
-        do_magnetisation && @info("Magnetization: $(magnetization)")
-        @info("rho ees: $rhoees")
+        @info("Energy error: $(energy - exact_energy)")
+        @info("Density matrix entropies: $rhoees")
         @info("Scaling dimensions: $scaldims")
+        return m, energy, g
     end
+
+    # The main loop, that for every vector space in `V_virtual_steps` optimises the MERA.
+    # For the first one, we use a randomly initialised MERA as the starting point, for the
+    # latter ones we expand the bond dimension by padding tensors with zeros.
+    local m
+    for i in 1:length(V_virtual_steps)
+        V_virtual = V_virtual_steps[i]
+        if i == 1
+            @info("Creating a random MERA with bond dimension $(dim(V_virtual)).")
+            # The lowest index is of the physical bond dimension, the others are virtual.
+            Vs = (V_physical, ntuple(x -> V_virtual, layers-1)...)
+            m = random_MERA(meratype, Float64, Vs)
+        else
+            @info("Expanding to bond dimension $(dim(V_virtual)).")
+            for j in 2:layers
+                m = expand_bonddim(m, j-1, V_virtual)
+            end
+        end
+        # Many more optimisation parameters can be specified, see the documentation on
+        # `minimize_expectation`.
+        pars = (gradient_delta = gradient_delta,
+                maxiter = maxiter_steps[i],
+                method = method,
+                verbosity = verbosity)
+        m = minimize_expectation(m, h, pars; finalize! = finalize!)
+    end
+
+    # Write the resulting MERA to disc. It can then be read using
+    # `DemoTools.load_mera(path)`.
+    DemoTools.store_mera(path, m)
 end
 
+# Run the actual script. The whole thing is in a function mainly to avoid polluting the
+# global namespace.
 main()
--- demo/demo_tools.jl
+++ demo/demo_tools.jl
@@ -1,28 +1,20 @@
-# Utilities for demo.jl and and demo_refine.jl.
-
+# Utilities for demo.jl
 module DemoTools
 
-using Logging
 using LinearAlgebra
 using TensorKit
 using JLD2
 using MERA
 
-export setlogger
-export expect, minimize_expectation, densitymatrix_entropies, scalingdimensions, remove_symmetry
-export load_mera, store_mera
-export build_H_Ising, build_H_XXZ, build_magop
-export get_optimized_mera
-
-# # # Functions for creating Hamiltonians.
-
 """
-Take a two-site operator `op` that defines the local term of a global operator, and block
-sites together to form a new two-site operator for which each site corresponds to
-`num_sites` sites of the original operator, and which sums up to the same global operator.
-`num_sites` should be a power of 2.
+    block_op(op, num_sites)
+
+Take an operator `op` that defines the local term of a global operator, and block sites
+together to form a new operator for which each site corresponds to `num_sites` sites of the
+original operator, and which sums up to the same global operator. `num_sites` should be a
+power of 2.
 """
-function block_op(op::SquareTensorMap{2}, num_sites)
+function block_op(op::MERA.SquareTensorMap{2}, num_sites)
     while num_sites > 1
         VL = space(op, 1)
         VR = space(op, 2)
@@ -45,13 +37,7 @@ function block_op(op::SquareTensorMap{2}, num_sites)
     return op
 end
 
-"""
-Take a one-site operator `op` that defines the local term of a global operator, and block
-sites together to form a new one-site operator for which each site corresponds to
-`num_sites` sites of the original operator, and which sums up to the same global operator.
-`num_sites` should be a power of 2.
-"""
-function block_op(op::SquareTensorMap{1}, num_sites)
+function block_op(op::MERA.SquareTensorMap{1}, num_sites)
     while num_sites > 1
         V = space(op, 1)
         eye = id(V)
@@ -72,14 +58,18 @@ function block_op(op::SquareTensorMap{1}, num_sites)
 end
 
 """
+    XXZ_hamiltonian(J_xy, J_z; symmetry=:none, block_size=1)
+
 Return the local Hamiltonian term for the XXZ model: J_xy*XX + J_xy*YY + J_z*ZZ.
-`symmetry` should be "none" or "group", and determmines whether the Hamiltonian should be an
-explicitly U(1) symmetric TensorMap or a dense one. `block_size` determines how many sites
-to block together, and should be a power of 2. If sites are blocked, then the Hamiltonian is
-divided by a constant to make the energy per site be the same as for the original.
+
+`symmetry` should be `:none` or `:group`, and determmines whether the Hamiltonian should be
+an explicitly U(1) symmetric `TensorMap` or a dense one. `block_size` determines how many
+sites to block together, and should be a power of 2. If sites are blocked, then the
+Hamiltonian is divided by a constant to make the energy per site be the same as for the
+original.
 """
-function build_H_XXZ(J_xy, J_z; symmetry="none", block_size=1)
-    if symmetry == "U1" || symmetry == "group"
+function XXZ_hamiltonian(J_xy, J_z; symmetry=:none, block_size=1)
+    if symmetry == :U1 || symmetry == :group
         V = ℂ[U₁](-1=>1, 1=>1)
         Z = TensorMap(zeros, Float64, V ← V)
         Z.data[U₁(1)] .= 1.0
@@ -87,7 +77,7 @@ function build_H_XXZ(J_xy, J_z; symmetry="none", block_size=1)
         ZZ = Z ⊗ Z
         XXplusYY = TensorMap(zeros, Float64, V ⊗ V ← V ⊗ V)
         XXplusYY.data[U₁(0)] .= [0.0 2.0; 2.0 0.0]
-    elseif symmetry == "none"
+    elseif symmetry == :none
         V = ℂ^2
         Z = TensorMap(zeros, Float64, V ← V)
         Z.data .= [1.0 0.0; 0.0 -1.0]
@@ -96,7 +86,7 @@ function build_H_XXZ(J_xy, J_z; symmetry="none", block_size=1)
         XXplusYY.data[2,3] = 2.0
         XXplusYY.data[3,2] = 2.0
     else
-        error("Unknown symmetry $symmetry")
+        error("Unknown symmetry: $symmetry")
     end
     H = J_xy*XXplusYY + J_z*ZZ
     H = block_op(H, block_size)
@@ -105,15 +95,18 @@ function build_H_XXZ(J_xy, J_z; symmetry="none", block_size=1)
 end
 
 """
+    ising_hamiltonian(h=1.0; symmetry=:none, block_size=1)
+
 Return the local Hamiltonian term for the Ising model: -XX - h*Z
-`symmetry` should be "none", "group", or "anyon" and determmines whether the Hamiltonian
-should be an explicitly Z2 symmetric or anyonic TensorMap, or a dense one. `block_size`
+
+`symmetry` should be `:none`, `:group`, or `:anyons` and determmines whether the Hamiltonian
+should be an explicitly Z2 symmetric or anyonic `TensorMap`, or a dense one. `block_size`
 determines how many sites to block together, and should be a power of 2. If sites are
 blocked, then the Hamiltonian is divided by a constant to make the energy per site be the
 same as for the original.
 """
-function build_H_Ising(h=1.0; symmetry="none", block_size=1)
-    if symmetry == "Z2"
+function ising_hamiltonian(h=1.0; symmetry=:none, block_size=1)
+    if symmetry == :Z2
         V = ℂ[ℤ₂](0=>1, 1=>1)
         # Pauli Z
         Z = TensorMap(zeros, Float64, V ← V)
@@ -127,12 +120,12 @@ function build_H_Ising(h=1.0; symmetry="none", block_size=1)
         XX.data[ℤ₂(0)] .= [0.0 1.0; 1.0 0.0]
         XX.data[ℤ₂(1)] .= [0.0 1.0; 1.0 0.0]
         H = -(XX + h/2 * (ZI+IZ))
-    elseif symmetry == "anyons"
+    elseif symmetry == :anyons
         V = RepresentationSpace{IsingAnyon}(:I => 0, :ψ => 0, :σ => 1)
         H = TensorMap(zeros, Float64, V ⊗ V ← V ⊗ V)
         H.data[IsingAnyon(:I)] .= 1.0
         H.data[IsingAnyon(:ψ)] .= -1.0
-    elseif symmetry == "none"
+    elseif symmetry == :none
         V = ℂ^2
         # Pauli matrices
         X = TensorMap(zeros, Float64, V ← V)
@@ -145,7 +138,7 @@ function build_H_Ising(h=1.0; symmetry="none", block_size=1)
         IZ = eye ⊗ Z
         H = -(XX + h/2 * (ZI+IZ))
     else
-        error("Unknown symmetry $symmetry")
+        error("Unknown symmetry: $symmetry")
     end
     H = block_op(H, block_size)
     H = H / block_size
@@ -153,210 +146,57 @@ function build_H_Ising(h=1.0; symmetry="none", block_size=1)
 end
 
 """
+    magop(; symmetry = :none, block_size = 1)
+
 Return the magnetization operator for the Ising model, blocked over `block_size` sites.
 """
-function build_magop(;block_size=1)
-    V = ℂ^2
+function magop(; symmetry = :none, block_size = 1)
+    if symmetry == :none 
+        V = ℂ^2
+    elseif symmetry == :group || symmetry == :Z2
+        V = Z2Space(0 => 1, 1 => 1)
+    else
+        msg = "Unknown symmetry: $symmetry"
+        raise(ArgumentError(msg))
+    end
     X = TensorMap(zeros, Float64, V ← V)
-    eye = id(V)
-    X.data .= [0.0 1.0; 1.0 0.0]
+    if symmetry == :none
+        X.data .= [0.0 1.0; 1.0 0.0]
+    end
     magop = block_op(X, block_size)
     return magop
 end
 
-# # # Functions for reading and writing to disk.
+"""
+    store_mera(path, m)
 
+Store the MERA `m` on disk at `path`.
+
+The storage format is a JLD2 file of `pseudoserialize(m)`.  We use pseudoserialization to
+both work around limitations of JLD(2) and improve compatibility between different versions
+of MERA.jl
+
+See also: [`load_mera`](@ref)
+"""
 function store_mera(path, m)
-    # TODO JLD2 still fails to store MERAs properly. Once that's fixed, switch away from
-    # using pseudoserialization.
-    #@save path m
     deser = pseudoserialize(m)
     @save path deser
 end
 
+"""
+    load_mera(path)
+
+Load the MERA at stored at `path`.
+
+The storage format is a JLD2 file of `pseudoserialize(m)`.  We use pseudoserialization to
+both work around limitations of JLD(2) and improve compatibility between different versions
+of MERA.jl
+
+See also: [`store_mera`](@ref)
+"""
 function load_mera(path)
-    # TODO JLD2 still fails to store MERAs properly. Once that's fixed, switch away from
-    # using pseudoserialization.
-    #@load path m
     @load path deser
     m = depseudoserialize(deser...)
-    return m
-end
-
-# # # Functions for optimizing a MERA.
-
-"""
-Given a VectorSpace `V`, return a list of sectors in which it might make sense to increase
-the dimension of `V`, if we want to increase the total dimension. This means all sectors of
-`V`, but possible some other sectors as well.
-"""
-function sectors_to_expand(V)
-    result = Set(sectors(V))
-    if typeof(V) == U₁Space
-        # The `collect` makes a copy, so that we don't iterate over the ones just added.
-        for s in collect(result)
-            # TODO The following is specific to XXZ: We make jumps by twos, as because of
-            # the Hamiltonian the odd sectors are useless.
-            splus = U₁(s.charge+2)
-            sminus = U₁(s.charge-2)
-            push!(result, splus)
-            push!(result, sminus)
-        end
-    end
-    return result
-end
-
-"""
-For a MERA `m` and its layer number `i`, we want to increase its bond dimension (the input
-dimension) to `chi`. To do that, we may have to decide on which symmetry sector to increase
-the dimension in. For that purpose, go through the possible symmetry sectors, try increasing
-each one, and optimize a bit too see how much it helps bring down the energy. Choose the
-symmetry sector that benefits the energy the most. Return the expanded and slightly
-optimized MERA.
-"""
-function expand_best_sector(m, i, chi, h, opt_pars)
-    V = inputspace(m, i)
-    d = dim(V)
-    expanded_meras = Dict()
-    # Parameters for an optimisation the only point of which is to restore isometricity.
-    isometrisation_pars = merge(opt_pars, (method = :ev,
-                                           maxiter = 1,
-                                           isometries_only_iters = 0,))
-    for s in sectors_to_expand(V)
-        # Expand the bond dimension of symmetry sector s and try optimizing a bit to see
-        # how useful this expansion is.
-        ms = deepcopy(m)
-        ds = dim(V, s)
-        chi_s = ds + (chi - d)
-        istop = (i == num_translayers(ms))
-        ms = expand_bonddim(ms, i, Dict(s => chi_s); check_invar=!istop)
-        ms = expand_internal_bonddim(ms, i+1, Dict(s => chi_s))
-        # Just to make the MERA isometric again, after expanding bond dimensions.
-        ms = minimize_expectation(ms, h, isometrisation_pars)
-        msg = "Expanded layer $i to bond dimenson $chi_s in sector $s."
-        @info(msg)
-        ms = minimize_expectation(ms, h, opt_pars)
-        expanded_meras[s] = ms
-    end
-    expanded_meras_array = collect(expanded_meras)
-    # Of the MERAs that got different symmetry sectors expanded, pick the one that has
-    # the smallest energy.
-    minindex = argmin([expect(h, pair[2]) for pair in expanded_meras_array])
-    s, m = expanded_meras_array[minindex]
-    msg = "Expanding sector $s yielded the lowest energy, keeping that.\nOutput spaces now:"
-    for i in 1:num_translayers(m)+1
-        msg *= "\nLayer $(i): $(outputspace(m, i))"
-    end
-    @info(msg)
-    return m
-end
-
-"""
-Return the smallest vector space with the given symmetry (either "none", "U1", or "Z2", at
-the moment) that makes sense as a index space of a MERA for the given model (either "Ising"
-or "XXZ"). For instance, the Ising model should have a non-zero bond dimension in both
-symmetry sectors, so the minimal space for Ising and Z2 is ComplexSpace(0 => 1, 1 => 1).
-"""
-function minimal_space(model, symmetry)
-    if symmetry == "none"
-        V = ℂ^1
-    elseif model == "XXZ" && symmetry == "U1"
-        V = ℂ[U₁](-2=>1, 0=>1, 2=>1)
-    elseif model == "Ising" && symmetry == "Z2"
-        V = ℂ[ℤ₂](0=>1, 1=>1)
-    else
-        error("Unknown symmetry $symmetry")
-    end
-    return V
-end
-
-"""
-Get a MERA for the given model, optimized with the parameters `pars`. If the requested MERA
-is already stored on disk, load and return it. If not, optimize for it, store the result on
-disk for future use, and return it. This function often recursively calls itself, because
-MERAs with lower bond dimensions are used as starting points for the optimisation of the
-higher-bond-dimension ones.
-"""
-function get_optimized_mera(datafolder, model, pars)
-    chi = pars[:chi]
-    layers = pars[:layers]
-    symmetry = pars[:symmetry]
-    block_size = pars[:block_size]
-    threads = pars[:threads]
-    BLAS.set_num_threads(threads)
-    meratypestr = pars[:meratype]
-    if meratypestr == "binary"
-        meratype = BinaryMERA
-    elseif meratypestr == "ternary"
-        meratype = TernaryMERA
-    elseif meratypestr == "modifiedbinary"
-        meratype = ModifiedBinaryMERA
-    else
-        msg = "Unknown MERA type: $(meratypestr)"
-        throw(ArgumentError(msg))
-    end
-
-    # The path to where this MERA should be stored.
-    mkpath(datafolder)
-    filename = "MERA_$(model)_$(meratypestr)_$(chi)_$(block_size)_$(symmetry)_$(layers)"
-    path = "$datafolder/$filename.jlm"
-
-    if isfile(path)
-        @info("Found $filename on disk, loading it.")
-        m = load_mera(path)
-        return m
-    end
-
-    @info("Did not find $filename on disk, generating it.")
-    # Build the Hamiltonian.
-    if model == "XXZ"
-        h = build_H_XXZ(pars[:J_xy], pars[:J_z]; symmetry=symmetry,
-                        block_size=block_size)
-    elseif model == "Ising"
-        h = build_H_Ising(pars[:h]; symmetry=symmetry, block_size=block_size)
-    else
-        msg = "Unknown model: $(model)"
-        throw(ArgumentError(msg))
-    end
-
-    # Figure out whether we should start the optimization from a random MERA or a previous
-    # MERA.
-    V_minimal = minimal_space(model, symmetry)
-    if chi < dim(V_minimal)
-        msg = "chi = $chi is too small for a $(symmetry)-symmetric MERA for $(model) model."
-        throw(ArgumentError(msg))
-    elseif chi == dim(V_minimal)
-        # The requested bond dimension is the smallest that makes sense to do with this
-        # symmetry. So just create a random MERA and optimize that.
-        V_virt = V_minimal
-        V_phys = space(h, 1)
-        Vs = tuple(V_phys, repeat([V_virt], layers-1)...)
-        T = eltype(h)
-        m = random_MERA(meratype, T, Vs; random_disentangler=false)
-        # We run all three stages of the optimization for the initial bond dimensions as
-        # well. This doesn't really make sense by itself, but it ensures that all bond
-        # dimensions go through all the different stages, which is necessary for instance
-        # because pars[:initial_opt_pars] typically involves a starting part where
-        # disentanglers are not optimized, and we want to apply that here too.
-        m = minimize_expectation(m, h, pars[:initial_opt_pars])
-        m = minimize_expectation(m, h, pars[:mid_opt_pars])
-        m = minimize_expectation(m, h, pars[:final_opt_pars])
-    else
-        # The bond dimensions requested is larger than the smallest that makes sense to do.
-        # Get the MERA with a bond dimension one smaller to use as a starting point, expand
-        # its bond dimension, and optimize that one.
-        prevpars = deepcopy(pars)
-        prevpars[:chi] -= 1
-        m = get_optimized_mera(datafolder, model, prevpars)
-        # Expand the bond dimension of each layer in turn.
-        for i in 1:num_translayers(m)
-            m = expand_best_sector(m, i, chi, h, pars[:initial_opt_pars])
-            opt_pars = i == num_translayers(m) ? pars[:final_opt_pars] : pars[:mid_opt_pars]
-            m = minimize_expectation(m, h, opt_pars)
-        end
-    end
-
-    store_mera(path, m)
     return m
 end
 
